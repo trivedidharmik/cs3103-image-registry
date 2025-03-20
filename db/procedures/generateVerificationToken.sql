e0b3d17dc98a20e7a8055cbb7d2e8f5bbdab6c08
trivedidharmik <dmtrivedi2@gmail.com>
2025-03-20 19:45:25 -0300

    updated generateVerificationToken to return the user id and token after inserting.

--- db/procedures/generateVerificationToken.sql
+++ db/procedures/generateVerificationToken.sql
@@ -4,5 +4,6 @@ BEGIN
     DECLARE v_token VARCHAR(255);
     SET v_token = UUID();
     INSERT INTO VerificationTokens (userId, token, expiresAt) VALUES (pUserId, v_token, NOW() + INTERVAL 1 DAY);
+    SELECT pUserId AS userId, v_token AS token;
 END //
 DELIMITER ;
