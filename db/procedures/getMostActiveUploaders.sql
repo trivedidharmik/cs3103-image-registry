c609aa6a32f4a552fc60d2974009a9eec781da15
Dharmik Manishkumar Trivedi <dtrived1@remotelabm16.cs.unb.ca>
2025-04-06 17:42:41 -0300

    Procedure change

--- db/procedures/getMostActiveUploaders.sql
+++ db/procedures/getMostActiveUploaders.sql
@@ -5,8 +5,9 @@ BEGIN
         u.userId,
         u.username,
         COUNT(i.imageId) AS imageCount
-    FROM Images i WHERE isVisible = 'public'
+    FROM Images i
     JOIN Users u ON i.userId = u.userId
+    WHERE i.isVisible = 'public'
     GROUP BY u.userId
     ORDER BY imageCount DESC
     LIMIT 3;
